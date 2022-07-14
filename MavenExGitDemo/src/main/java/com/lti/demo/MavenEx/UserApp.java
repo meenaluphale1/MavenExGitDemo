2333befbf65eddabe9592ed6c0c8ad702a6d955c
MeenalAnand <meenaluphalegit@gmail.com>
2022-07-14 12:22:57 +0530

    second commit

--- MavenExGitDemo/src/main/java/com/lti/demo/MavenEx/UserApp.java
+++ MavenExGitDemo/src/main/java/com/lti/demo/MavenEx/UserApp.java
@@ -2,7 +2,8 @@ package com.lti.demo.MavenEx;
 
 public class UserApp {
 	public static void main(String[] args) {
-		System.out.println(" person clas");
+		
+		System.out.println(" person clas ...");
 	}
 
 }
